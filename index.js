79f153aeac79f96cc1b65e82d11b78d23d5b6ee8
sahib035 <sahibullah035@gmail.com>
2024-02-18 12:23:29 +0500

    add assignment task

---
+++ index.js
@@ -0,0 +1,19 @@
+//Queation 1
+
+// function displayWelcomeMessage(){
+//     let userName = prompt("enter userName");
+//     alert(`welcome ${userName}!`);
+// }
+// displayWelcomeMessage();
+
+
+
+
+//Queation 3
+function generateRandom(){
+    let rand = Math.floor(Math.random() * 100) + 1;
+    console.log(rand);
+  
+    return rand;
+  }
+  generateRandom();
