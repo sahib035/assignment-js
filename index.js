823294ee2f099129b6012f5e668a862ff844f4b9
sahib035 <sahibullah035@gmail.com>
2024-02-20 14:26:38 +0500

    modified code and add somra extra Questions

--- index.js
+++ index.js
@@ -6,14 +6,24 @@
 // }
 // displayWelcomeMessage();
 
+// Question 2
+function generateQuote() {
+  let quote = [
+    "Education is key to success",
+    "You must be the change you wish to see in the world. ",
+    "well done is better than well said ",
+    "In the end it's not the  years in your life that count it's life in your years",
+  ];
+  let randomNumber = Math.floor(Math.random() * quote.length);
+  alert(quote[randomNumber])
+}
+generateQuote();
 
+// Queation 3
+// function generateRandom(){
+//     let rand = Math.floor(Math.random() * 100) + 1;
+//     console.log(rand);
 
-
-//Queation 3
-function generateRandom(){
-    let rand = Math.floor(Math.random() * 100) + 1;
-    console.log(rand);
-  
-    return rand;
-  }
-  generateRandom();
+//     return rand;
+//   }
+//   generateRandom();
